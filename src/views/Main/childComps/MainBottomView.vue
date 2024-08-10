2d8e2fd4c49ddd4e03101bc69f7f12601f187364
hqchqc <734986933@qq.com>
2024-08-10 12:30:22 +0800

    fix: text cd

--- src/views/Main/childComps/MainBottomView.vue
+++ src/views/Main/childComps/MainBottomView.vue
@@ -4,7 +4,7 @@
   <div
     class="absolute bottom-0 left-0 m-auto h-11 w-full flex items-center bg-[#00000040] text-white"
   >
-    <p class="m-auto max-w-xl">Copyright 2024 & Power by imsyy & 浙ICP备20006869号-1</p>
+    <p class="m-auto max-w-xl">Copyright 2023-2024 & Power by imsyy & 浙ICP备20006869号-1</p>
   </div>
 </template>
 
